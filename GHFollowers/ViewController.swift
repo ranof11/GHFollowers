c7af8b9a04c965d5d019dc9c26ed127b26d4bb8b
ranof11 <rajeshtriadi14@gmail.com>
2025-03-30 17:35:11 +0700

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Rajesh Triadi Noftarizal on 30/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
